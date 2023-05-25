4d5b35df5167a41c98a658ce44930b89cdde4f76
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-25 14:18:50 +0900

    added click event handler, needs refactoring to more appropriate event listener

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const GRID_SIZE = 600;
 
-
 const sketchArea = document.querySelector('.sketch-container');
-console.log(sketchArea);
+
+createGrid(16);
 
 function createGrid(n) {
+    sketchArea.replaceChildren();
     for (let i = 0; i < (n * n); i ++) {
         const grid = document.createElement('div');
         grid.className = 'sketch';
@@ -15,4 +16,19 @@ function createGrid(n) {
     }
 }
 
-createGrid(16);
+const slider = document.querySelector(".slider");
+const sliderDisplay = document.querySelector(".slider-display");
+slider.addEventListener('input', () => {
+    sliderDisplay.textContent = `${slider.value} x ${slider.value}`;
+    createGrid(slider.value);
+});
+
+function color(element) {
+    element.style.backgroundColor = 'black';
+}
+
+sketchArea.childNodes.forEach((element) => {
+    element.addEventListener('click', () => {
+        color(element);
+    });
+});
